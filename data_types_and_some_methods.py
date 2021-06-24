ce3c2ea548b540abbe9d7972779461311e0b3f7c
Jacopo Rodighiero <jacoporodighiero@Jacopos-MacBook-Pro-2.local>
2021-06-24 13:22:21 +0200

    first commit

---
+++ data_types_and_some_methods.py
@@ -0,0 +1,18 @@
+parrot = 'norwegian blue'
+
+print(parrot)
+
+#prints the letters of the string starting at 0
+print(parrot[3], parrot[4], parrot[9], parrot[3], parrot[6], parrot[8])
+
+#prints the letters of the string starting from the last one
+print(parrot[-1])
+
+#slice letter from 0 to 6 (6 is not included!), norweg
+print(parrot[0:6])
+#also, if not included the default start is position 0
+print(parrot[:9])
+
+#also, if not included the default finish is the last letter
+print(parrot[10:])
+
